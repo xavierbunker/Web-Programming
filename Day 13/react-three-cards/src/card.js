0783eb931e82adf20868d16ea89ddbd314f1d09f
xavierbunker <xavier.l.bunker@gmail.com>
2025-02-26 22:09:17 -0600

    new updated

---
+++ Day 13/react-three-cards/src/card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Card.css"; //Import CSS for styling
+
+function Card({title, content, color}) {
+  return (
+    <div className="card" style={{ backgroundColor: color }}>
+      <h2>{title}</h2>
+      <p>{content}</p>
+    </div>
+  );
+}
+
+export default Card;
