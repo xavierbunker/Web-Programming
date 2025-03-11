10d82672c86cbc5de5f64f51545769306dad7ee1
xavierbunker <xavier.l.bunker@gmail.com>
2025-03-10 21:33:44 -0500

    hard task

--- Day 15/mood-tracker/src/MoodTracker.js
+++ Day 15/mood-tracker/src/MoodTracker.js
@@ -4,10 +4,26 @@ export default function MoodTracker() {
 
     const [mood, setMood] = useState("😊");
 
+    const moodDetails = {
+        "😂": {description: 'Very Happy',link:'',memory:'When I Got My Jeep'},
+        "😒": {description: 'Unamused',link:'',memory:'When Getting On My Nerves'},
+        "😢": {description: 'Sad',link:'https://youtu.be/9bFHsd3o1w0?si=auyizKCtYXUDiw14',memory:'When I lost Puggles'},
+        "😡": {description: 'Angry',link:'',memory:'Almost Getting Hit By A Car'},
+        "😴": {description: 'Sleepy',link:'',memory:'Everyday Since Turning 21'},
+        "🤔": {description: 'Confused',link:'',memory:'School Work'},
+        "🤩": {description: 'Excited',link:'',memory:'When I Got My Driver License'},
+        "💕": {description: 'Love',link:'',memory:'People And Things Close To Me'}
+    };
+
     return (
         <div style={{ textAlign: "center", padding: "20px" }}>
             <h1>Mood Tracker</h1>
             <p>Current Mood: {mood}</p>
+            <>
+            <p>{moodDetails[mood]?.description}</p>
+            <a href={moodDetails[mood]?.link}>Listen to a song</a>
+            <p>{moodDetails[mood]?.memory}</p>
+            </>
             <button onClick={() => setMood("😂")} style={{margin: "5px",padding: "10px",background: "lightgreen"}}>Very Happy</button>
             <button onClick={() => setMood("😒")} style={{margin: "5px",padding: "10px",background: "lightblue"}}>Unamused</button>
             <button onClick={() => setMood("😢")} style={{margin: "5px",padding: "10px",background: "lightcoral"}}>Sad</button>
