31a195f35f15eef6ad836918cc270e94cdfd8041
xavierbunker <xavier.l.bunker@gmail.com>
2025-04-16 18:51:33 -0500

    working on a fix

--- Final Project/information/src/App.jsx
+++ Final Project/information/src/App.jsx
@@ -1,3 +1,4 @@
+// App.jsx (Corrected)
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import axios from 'axios'
@@ -15,27 +16,27 @@ function NavBar() {
     <nav className="bg-gray-800 p-4">
       <ul className="flex space-x-4">
         <li>
-          <Link to="./" className="text-white hover:text-gray-400">
+          <Link to="/" className="text-white hover:text-gray-400">
             Home
           </Link>
         </li>
         <li>
-          <Link to="./projects" className="text-white hover:text-gray-400">
+          <Link to="/projects" className="text-white hover:text-gray-400">
             Projects
           </Link>
         </li>
         <li>
-          <Link to="./education" className="text-white hover:text-gray-400">
+          <Link to="/education" className="text-white hover:text-gray-400">
             Education
           </Link>
         </li>
         <li>
-          <Link to="./skills" className="text-white hover:text-gray-400">
+          <Link to="/skills" className="text-white hover:text-gray-400">
             Skills
           </Link>
         </li>
         <li>
-          <Link to="./contact" className="text-white hover:text-gray-400">
+          <Link to="/contact" className="text-white hover:text-gray-400">
             Contact
           </Link>
         </li>
@@ -50,11 +51,11 @@ export default function App() {
       <div className="container mx-auto p-4">
         <NavBar />
         <Routes>
-          <Route path="./" element={<Home />} />
-          <Route path="./projects" element={<Projects />} />
-          <Route path="./education" element={<Education />} />
-          <Route path="./skills" element={<Skills />} />
-          <Route path="./contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </Router>
